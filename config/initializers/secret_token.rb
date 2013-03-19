60a42bec87744f78cadd3e0fe44c3a79603945db
Joao Eduardo Montandon <edu.montandon@gmail.com>
2013-03-19 10:47:04 -0300

    Initial commit. Project setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeeklyReport::Application.config.secret_token = '2a7862efc384ccdeec137926b5e3795cdedbd52ee5ab173c893f6e054c3655e8e4979c48d68d4996f90f5466a048466f0917eb45cef2170b91c15c4770a2c67a'
